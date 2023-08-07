7053d4d5421ed3c958a7a32f5151ef43de3a1284
mahesh9561 <maheshpathak200@gmail.com>
2023-08-08 04:10:46 +0530

    datatypes & memory

---
+++ 01_Basics/datatypes-summery.js
@@ -0,0 +1,15 @@
+// Primitive DataTypes - call by value
+// 7-Types
+// 1. String 
+// 2. Number
+// 3. Booleans
+// 4. Undefined
+// 5. Symbols
+// 6. BigInt
+
+
+// Non =-Premitive Datatyoes - Referance Type
+
+// Array
+// Objects
+// Functions
