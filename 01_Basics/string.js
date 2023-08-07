89f9574a370238209cd499c8809bb57adf39c697
mahesh9561 <maheshpathak200@gmail.com>
2023-08-08 04:52:15 +0530

    String

---
+++ 01_Basics/string.js
@@ -0,0 +1,9 @@
+const name = "Mahesh";
+const repoCount = 50;
+
+// console.log(name + repoCount + "Value");
+// String interpolation
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Mahesh');
+console.log(gameName);
