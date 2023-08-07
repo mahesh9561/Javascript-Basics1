7d687ad1310348fa34fb1fa4956b2e54fdad22a2
mahesh9561 <maheshpathak200@gmail.com>
2023-08-08 04:11:47 +0530

    memory

--- 01_Basics/datatypes-summery.js
+++ 01_Basics/datatypes-summery.js
@@ -9,7 +9,36 @@
 
 
 // Non =-Premitive Datatyoes - Referance Type
-
 // Array
 // Objects
 // Functions
+
+
+// =================================================== //
+// =================Memory========================
+// stack (Primitive Memory)
+// Heap (Non-Primitive) - Referance
+
+let myName = "Mahesh Pathak";
+let anotherName = myName;
+
+anotherName = "Vivek Pathak";
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email : "mahesh@gmail.com",
+    upi: "mahesh@ybl"
+}
+
+let userTwo = {
+    email : "vivek@gmail.com",
+    upi: "vivek@ybl"
+}
+
+userTwo = userOne;
+userTwo.email = "xyz@gmail.com"
+
+console.log(userOne);
+console.log(userTwo);
