16f1dbc224b04808c9d700325d7b85848ff4147f
mahesh9561 <maheshpathak200@gmail.com>
2023-08-07 13:07:29 +0530

    Deleted file dataType.js

---
+++ 01_Basics/converstion.js
@@ -0,0 +1,21 @@
+let score = 33;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// 33 => 33
+// "33abs" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Mahesh" => true
---
+++ 01_Basics/dataType.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as a new version
+
+//alert(3 + 3); // we are using nodejs, not browser
+
+//code redeability should be high 
+
+// DataTypes
+// Number => 2,4
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => undefined
+// symbol => unique
+
+
+// Object
+
+
+console.log(typeof undefined);
+console.log(typeof null);
