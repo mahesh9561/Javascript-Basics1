1f545cb53fc56450d0e750518cf78f0032af099a
mahesh9561 <maheshpathak200@gmail.com>
2023-08-09 13:19:21 +0530

    .

---
+++ 01_Basics/07_datesin_js.js
@@ -0,0 +1,13 @@
+// Dates
+
+let myDate = new  Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+let myCreateDate = new Date(2023, 0, 23);
+console.log(myCreateDate);
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
