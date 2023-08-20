4debfa5952a269af888c8f0dfbc9860599d90499
mahesh9561 <maheshpathak200@gmail.com>
2023-08-20 13:52:35 +0530

    Itreation

---
+++ 05_Interations/02_While.js
@@ -0,0 +1,13 @@
+let index = 0;
+// while (i == 2) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2;
+//     console.log(index);
+// }
+
+let myArray = ['flash',"batman","superman"];
+let arr = 0;
+while (arr < myArray.length){
+    console.log(`Value is ${myArray.length}`);
+    arr = arr +1;
+}
